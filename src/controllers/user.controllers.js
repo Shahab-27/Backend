a51d9068d02021e4ad9cb3e1a7c2dc54e1154ff9
shahab_27 <shahabfardeen2019@gmail.com>
2025-07-25 19:34:12 +0530

    updated user.controller.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,36 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler(async(req,res) =>{
-    res.status(200).json({
-        message : "ok"
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+     const {fullName, email, username, password } = req.body
+    //console.log("email: ", email);
+    const fields = [fullName, email, username, password];
+
+    for(const field of fields){
+        if(!field || field.trim() === ""){
+            throw new ApiError(400, "All fields are required");
+        }
+    }
+
+     const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
+
+     if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+    
 })
 
 export {registerUser}
