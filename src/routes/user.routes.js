327718b5a0b30fc59617a8c32f327f7a7b8ed2ba
shahab_27 <shahabfardeen2019@gmail.com>
2025-07-28 18:07:14 +0530

    added logout functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser,loginUser, logoutUser, } from "../controllers/user.controllers.js";
 import {upload} from "../middleware/multer.middleware.js"
 const router = Router();
 
@@ -16,5 +16,13 @@ router.route("/register").post(
     ]),
     registerUser
     )
+
+    
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+
+
 
 export default router;
