d0a1627eaf06461136785e60657d1aa3ac04dfe8
shahab_27 <shahabfardeen2019@gmail.com>
2025-07-29 19:45:17 +0530

    Minor typos corrected

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -111,7 +111,7 @@ const loginUser = asyncHandler(async (req, res) =>{
     }
 
     const {accessToken, refreshToken} =
-     await generateAccessAndRefreshTokens(user._id)
+     await generateRefreshAndAccessToken(user._id)
 
     const loggedInUser = await User.findById(user._id)
     .select("-password -refreshToken")
