6e37f84ec03d62457fea7a5969fb0b388a49249b
shahab_27 <shahabfardeen2019@gmail.com>
2025-08-05 18:14:44 +0530

    updated routes comments for clarity

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -40,7 +40,10 @@ router.route("/refresh-token").post(refreshAccessToken)
 // inserted middleware so that verified user can change password
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+// patch is use so that only specific things gets updated
+// You want to update only specific fields, not the whole object.
+
+    router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
